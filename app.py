b6fa3951fd3d3d98a44ade6f0cd98cef8d6fac0d
tangchuan <tangchuan@meiqia.com>
2024-05-30 18:16:41 +0800

    支持上传模型，以及加入html页面

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-import torch
+import os
+
 import ChatTTS
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, send_file, jsonify, Response
 import io
 import soundfile as sf
 
 app = Flask(__name__)
 chat = ChatTTS.Chat()
-chat.load_models()
+model_source = os.getenv('MODEL_SOURCE', "huggingface")
+model_path = os.getenv('MODEL_PATH', "models")
+chat.load_models(source=model_source, local_path=model_path)
+
+
+@app.route('/')
+def home():
+    return send_file('tts.html')
+
 
 @app.route('/tts', methods=['GET'])
 def text_to_speech():
@@ -18,7 +27,8 @@ def text_to_speech():
     audio_buffer = io.BytesIO()
     sf.write(audio_buffer, wavs[0][0], 24000, format='WAV')
     audio_buffer.seek(0)
-    return Response(audio_buffer,mimetype="audio/wav")
+    return Response(audio_buffer, mimetype="audio/wav")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8889)
